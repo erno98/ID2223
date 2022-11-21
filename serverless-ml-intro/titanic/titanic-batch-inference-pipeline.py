6304f7e483550986a8f06626286d0e1c22f6674b
Anton Danker <anton.danker@gmail.com>
2022-11-21 20:02:33 +0100

    Completion of feature & training & batch inferernce pipelines

--- serverless-ml-intro/titanic/titanic-batch-inference-pipeline.py
+++ serverless-ml-intro/titanic/titanic-batch-inference-pipeline.py
@@ -1,14 +1,17 @@
 import os
 import modal
-    
-LOCAL=True
+
+LOCAL = True
 
 if LOCAL == False:
-   stub = modal.Stub()
-   hopsworks_image = modal.Image.debian_slim().pip_install(["hopsworks==3.0.4","joblib","seaborn","sklearn","dataframe-image"])
-   @stub.function(image=hopsworks_image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
-   def f():
-       g()
+    stub = modal.Stub()
+    hopsworks_image = modal.Image.debian_slim().pip_install(
+        ["hopsworks==3.0.4", "joblib", "seaborn", "sklearn", "dataframe-image"])
+
+    @stub.function(image=hopsworks_image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
+    def f():
+        g()
+
 
 def g():
     import pandas as pd
@@ -23,79 +26,78 @@ def g():
     import seaborn as sns
     import requests
 
-    project = hopsworks.login()
+    project = hopsworks.login(project="ID2223_Anton")
     fs = project.get_feature_store()
     dataset_api = project.get_dataset_api()
-    
+
     mr = project.get_model_registry()
     model = mr.get_model("titanic_modal", version=1)
     model_dir = model.download()
     model = joblib.load(model_dir + "/titanic_model.pkl")
-    
-    feature_view = fs.get_feature_view(name="titanic_modal", version=1)
+
+    feature_view = fs.get_feature_view(name="titanic_surv_modal", version=1)
     batch_data = feature_view.get_batch_data()
-    
+
     y_pred = model.predict(batch_data)
-    #print(y_pred)
+    # print(y_pred)
     offset = 1
     survived = y_pred[y_pred.size-offset]
-    print("Survabiliuty predicted: " + survived)
+    print("Survabiliuty predicted: " + str(survived))
 
-   
-    iris_fg = fs.get_feature_group(name="titanic_modal", version=1)
-    df = iris_fg.read() 
-    #print(df)
-    label = df.iloc[-offset]["Survived"]
-    
+    iris_fg = fs.get_feature_group(name="titanic_surv_modal", version=1)
+    df = iris_fg.read()
+    # print(df)
+    label = df.iloc[-offset]["survived"]
+
     monitor_fg = fs.get_or_create_feature_group(name="titanic_predictions",
                                                 version=1,
                                                 primary_key=["datetime"],
                                                 description="Titanic Survival Prediction/Outcome Monitoring"
                                                 )
-    
+
     now = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     data = {
         'prediction': [survived],
         'label': [label],
         'datetime': [now],
-       }
+    }
     monitor_df = pd.DataFrame(data)
-    monitor_fg.insert(monitor_df, write_options={"wait_for_job" : False})
-    
+    monitor_fg.insert(monitor_df, write_options={"wait_for_job": False})
+
     history_df = monitor_fg.read()
-    # Add our prediction to the history, as the history_df won't have it - 
+    # Add our prediction to the history, as the history_df won't have it -
     # the insertion was done asynchronously, so it will take ~1 min to land on App
     history_df = pd.concat([history_df, monitor_df])
 
-
     df_recent = history_df.tail(4)
-    dfi.export(df_recent, './df_recent.png', table_conversion = 'matplotlib')
+    dfi.export(df_recent, './df_recent.png', table_conversion='matplotlib')
     dataset_api.upload("./df_recent.png", "Resources/images", overwrite=True)
-    
+
     predictions = history_df[['prediction']]
     labels = history_df[['label']]
 
     # Only create the confusion matrix when our iris_predictions feature group has examples of all 3 iris flowers
-    print("Number of different survival predictions to date: " + str(predictions.value_counts().count()))
+    print("Number of different survival predictions to date: " +
+          str(predictions.value_counts().count()))
     if predictions.value_counts().count() == 3:
         results = confusion_matrix(labels, predictions)
-    
+
         df_cm = pd.DataFrame(results, ['True Died', 'True Survived'],
                              ['Pred Died', 'Pred Survived'])
-    
+
         cm = sns.heatmap(df_cm, annot=True)
         fig = cm.get_figure()
         fig.savefig("./confusion_matrix.png")
-        dataset_api.upload("./confusion_matrix.png", "Resources/images", overwrite=True)
+        dataset_api.upload("./confusion_matrix.png",
+                           "Resources/images", overwrite=True)
     else:
         print("You need 2 different survival predictions to create the confusion matrix.")
-        print("Run the batch inference pipeline more times until you get 2 different survival predictions") 
+        print("Run the batch inference pipeline more times until you get 2 different survival predictions")
 
 
 if __name__ == "__main__":
-    if LOCAL == True :
+    if LOCAL == True:
         g()
     else:
         with stub.run():
             f()
-
