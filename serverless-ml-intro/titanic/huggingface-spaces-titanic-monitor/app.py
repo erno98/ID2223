7c88cbb5530b828e16a5ff76ba9fc6e4beacfae0
Ernest Pokropek <ernest.pokropek98@gmail.com>
2022-11-21 20:31:08 +0100

    added app for huggingface for titanic

--- serverless-ml-intro/titanic/huggingface-spaces-titanic-monitor/app.py
+++ serverless-ml-intro/titanic/huggingface-spaces-titanic-monitor/app.py
@@ -7,19 +7,19 @@ fs = project.get_feature_store()
 
 dataset_api = project.get_dataset_api()
 
-dataset_api.download("Resources/images/latest_iris.png")
-dataset_api.download("Resources/images/actual_iris.png")
+dataset_api.download("Resources/images/latest_passenger.png")
+dataset_api.download("Resources/images/actual_passenger.png")
 dataset_api.download("Resources/images/df_recent.png")
 dataset_api.download("Resources/images/confusion_matrix.png")
 
 with gr.Blocks() as demo:
     with gr.Row():
       with gr.Column():
-          gr.Label("Today's Predicted Image")
-          input_img = gr.Image("latest_iris.png", elem_id="predicted-img")
+          gr.Label("Today's Predicted survival of the passenger")
+          input_img = gr.Image("latest_passenger.png", elem_id="predicted-img")
       with gr.Column():          
-          gr.Label("Today's Actual Image")
-          input_img = gr.Image("actual_iris.png", elem_id="actual-img")        
+          gr.Label("Today's Actual survival of the passenger")
+          input_img = gr.Image("actual_passenger.png", elem_id="actual-img")        
     with gr.Row():
       with gr.Column():
           gr.Label("Recent Prediction History")
@@ -27,5 +27,6 @@ with gr.Blocks() as demo:
       with gr.Column():          
           gr.Label("Confusion Maxtrix with Historical Prediction Performance")
           input_img = gr.Image("confusion_matrix.png", elem_id="confusion-matrix")        
+  
 
 demo.launch()
