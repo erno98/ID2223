bc89053bd74b262d6147ec13f315c69c2e5f4f69
Anton Danker <anton.danker@gmail.com>
2022-11-21 19:15:45 +0100

    patched feature & training pipelines

--- serverless-ml-intro/titanic/titanic-feature-pipeline.py
+++ serverless-ml-intro/titanic/titanic-feature-pipeline.py
@@ -4,18 +4,19 @@ import modal
 import hopsworks
 import pandas as pd
 
-project = hopsworks.login()
+project = hopsworks.login(project="ID2223_Anton")
 fs = project.get_feature_store()
 
 titanic_df = pd.read_csv(
-    "https://raw.githubusercontent.com/erno98/ID2223/main/serverless-ml-intro/titanic/titanic_processed.csv")
+    "https://raw.githubusercontent.com/erno98/ID2223/main/serverless-ml-intro/titanic/titanic_processed.csv", index_col=0)
 
 titanic_fg = fs.get_or_create_feature_group(
-    name="titanic_modal",
+    name="titanic_surv_modal",
     version=1,
     primary_key=["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"],
     description="Titanic dataset")
 titanic_fg.insert(titanic_df, write_options={"wait_for_job": False})
+
 
 #expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")
 #value_between(expectation_suite, "sepal_length", 4.5, 8.0)
