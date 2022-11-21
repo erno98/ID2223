01c53616690185d5c890156f0dbe0bce0dfbbe18
Anton Danker <anton.danker@gmail.com>
2022-11-21 14:26:42 +0100

    Adapted Feature pipeline for Titanic Dataset

--- serverless-ml-intro/titanic/titanic-feature-pipeline.py
+++ serverless-ml-intro/titanic/titanic-feature-pipeline.py
@@ -7,20 +7,19 @@ import pandas as pd
 project = hopsworks.login()
 fs = project.get_feature_store()
 
-iris_df = pd.read_csv("https://repo.hops.works/master/hopsworks-tutorials/data/iris.csv")
+titanic_df = pd.read_csv(
+    "https://repo.hops.works/master/hopsworks-tutorials/data/iris.csv")
 
-iris_fg = fs.get_or_create_feature_group(
-    name="iris_modal",
+titanic_fg = fs.get_or_create_feature_group(
+    name="Titanic_modal",
     version=1,
-    primary_key=["sepal_length","sepal_width","petal_length","petal_width"], 
-    description="Iris flower dataset")
-iris_fg.insert(iris_df, write_options={"wait_for_job" : False})
+    primary_key=["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"],
+    description="Titanic dataset")
+titanic_fg.insert(titanic_df, write_options={"wait_for_job": False})
 
-#expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")    
+#expectation_suite = ge.core.ExpectationSuite(expectation_suite_name="iris_dimensions")
 #value_between(expectation_suite, "sepal_length", 4.5, 8.0)
 #value_between(expectation_suite, "sepal_width", 2.1, 4.5)
 #value_between(expectation_suite, "petal_length", 1.2, 7)
 #value_between(expectation_suite, "petal_width", 0.2, 2.5)
-#iris_fg.save_expectation_suite(expectation_suite=expectation_suite, validation_ingestion_policy="STRICT")    
-    
-
+#iris_fg.save_expectation_suite(expectation_suite=expectation_suite, validation_ingestion_policy="STRICT")
