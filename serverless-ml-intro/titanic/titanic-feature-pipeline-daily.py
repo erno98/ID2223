ed28661597cf058f6e041fb48f54e376c6b14230
Anton Danker <anton.danker@gmail.com>
2022-11-21 18:22:22 +0100

    Final iris inference and initial Titanic featue pipeline

--- serverless-ml-intro/titanic/titanic-feature-pipeline-daily.py
+++ serverless-ml-intro/titanic/titanic-feature-pipeline-daily.py
@@ -1,18 +1,19 @@
 import os
 import modal
 
-LOCAL=False
+LOCAL = False
 
 if LOCAL == False:
-   stub = modal.Stub("iris_daily")
-   image = modal.Image.debian_slim().pip_install(["hopsworks==3.0.4"]) 
+    stub = modal.Stub("Titanic Daily")
+    image = modal.Image.debian_slim().pip_install(["hopsworks==3.0.4"])
 
-   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("jim-hopsworks-ai"))
-   def f():
-       g()
+    @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("ID2223"))
+    def f():
+        print(os.environ["HOPSWORKS_API_KEY"])
+        g()
 
 
-def generate_flower(name, sepal_len_max, sepal_len_min, sepal_width_max, sepal_width_min, 
+def generate_flower(name, sepal_len_max, sepal_len_min, sepal_width_max, sepal_width_min,
                     petal_len_max, petal_len_min, petal_width_max, petal_width_min):
     """
     Returns a single iris flower as a single row in a DataFrame
@@ -20,13 +21,21 @@ def generate_flower(name, sepal_len_max, sepal_len_min, sepal_width_max, sepal_w
     import pandas as pd
     import random
 
-    df = pd.DataFrame({ "sepal_length": [random.uniform(sepal_len_max, sepal_len_min)],
+    df = pd.DataFrame({"sepal_length": [random.uniform(sepal_len_max, sepal_len_min)],
                        "sepal_width": [random.uniform(sepal_width_max, sepal_width_min)],
                        "petal_length": [random.uniform(petal_len_max, petal_len_min)],
                        "petal_width": [random.uniform(petal_width_max, petal_width_min)]
-                      })
+                       })
     df['variety'] = name
     return df
+
+
+def generate_passenger():
+    """
+    Returns single Titanic passenger as single row in a DataFrame
+    """
+
+    return 1
 
 
 def get_random_iris_flower():
@@ -36,12 +45,14 @@ def get_random_iris_flower():
     import pandas as pd
     import random
 
-    virginica_df = generate_flower("Virginica", 8, 5.5, 3.8, 2.2, 7, 4.5, 2.5, 1.4)
-    versicolor_df = generate_flower("Versicolor", 7.5, 4.5, 3.5, 2.1, 3.1, 5.5, 1.8, 1.0)
-    setosa_df =  generate_flower("Setosa", 6, 4.5, 4.5, 2.3, 1.2, 2, 0.7, 0.3)
+    virginica_df = generate_flower(
+        "Virginica", 8, 5.5, 3.8, 2.2, 7, 4.5, 2.5, 1.4)
+    versicolor_df = generate_flower(
+        "Versicolor", 7.5, 4.5, 3.5, 2.1, 3.1, 5.5, 1.8, 1.0)
+    setosa_df = generate_flower("Setosa", 6, 4.5, 4.5, 2.3, 1.2, 2, 0.7, 0.3)
 
     # randomly pick one of these 3 and write it to the featurestore
-    pick_random = random.uniform(0,3)
+    pick_random = random.uniform(0, 3)
     if pick_random >= 2:
         iris_df = virginica_df
         print("Virginica added")
@@ -64,11 +75,12 @@ def g():
 
     iris_df = get_random_iris_flower()
 
-    iris_fg = fs.get_feature_group(name="iris_modal",version=1)
-    iris_fg.insert(iris_df, write_options={"wait_for_job" : False})
+    iris_fg = fs.get_feature_group(name="iris_modal", version=1)
+    iris_fg.insert(iris_df, write_options={"wait_for_job": False})
+
 
 if __name__ == "__main__":
-    if LOCAL == True :
+    if LOCAL == True:
         g()
     else:
         stub.deploy("iris_daily")
